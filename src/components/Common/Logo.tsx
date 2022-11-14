7c92c947042867b5321a8ae5200283123a432e62
Subin S K <subinsk284@gmail.com>
2022-11-14 22:00:16 +0530

    done

--- src/components/Common/Logo.tsx
+++ src/components/Common/Logo.tsx
@@ -4,9 +4,9 @@ import { FC } from "react";
 export const Logo: FC<{}> = ({ }) => {
     return (
         <Image
-            src={"https://res.cloudinary.com/dli15zk4p/image/upload/v1668113897/c3-website/c3_without_bg_4x_wsnuxp.png"}
-            height={80}
-            width={80}
+            src={"https://res.cloudinary.com/dli15zk4p/image/upload/v1668146056/c3-website/c3_icon_without_bg_4x_kromg7.png"}
+            height={110}
+            width={110}
             alt="Logo"
         />
     )
--- src/components/Common/Navbar.tsx
+++ src/components/Common/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import { FC, useEffect, useState } from "react";
+import React, { FC, useEffect, useState } from "react";
+
 import { Logo } from "./Logo";
 import { MobileNavbar } from "./MobileNavbar";
 
@@ -25,13 +26,14 @@ export const Navbar: FC<{}> = ({
     useEffect(() => {
         window.addEventListener('scroll', changeBackground);
     }, [])
+
     return (
-        <div className={`sticky z-50 ${changeNavbar ? 'bg-white shadow-md' : 'shadow-none bg-[#f9fbfc]'} top-0 flex justify-between items-center h-16 p-2 px-7 w-full border-0`}>
+        <div className={`sticky z-50 ${changeNavbar ? 'bg-black/30 shadow-md' : 'shadow-none bg-transparent'} col-span-6 top-0 flex justify-between items-center h-[4rem] p-2 px-5 w-full border-0`}>
             <Link href={'/'}>
                 <Logo />
             </Link>
             <MobileNavbar showMobNavbar={mobNavbar} mobNavbarHandler={mobileNavbarHandler} />
-            <div className="flex font-semibold text-white items-center">
+            <div className="md:flex hidden font-semibold text-white items-center space-x-8">
                 <Link href={'/team'}>
                     <span className="cursor-pointer">
                         Team
@@ -43,7 +45,7 @@ export const Navbar: FC<{}> = ({
                     </span>
                 </Link>
             </div>
-            <svg onClick={() => mobileNavbarHandler(true)} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 block md:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg onClick={() => mobileNavbarHandler(true)} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 block md:hidden text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
         </div>
