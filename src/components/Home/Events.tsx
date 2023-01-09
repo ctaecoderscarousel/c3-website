abf43554cfc2584592ed70c49b1a9e8a642dc10e
himanisoni05 <himani.soni9099@gmail.com>
2023-01-09 21:12:21 +0530

    Event-Carousel

--- src/components/Home/Events.tsx
+++ src/components/Home/Events.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import { FC } from "react";
 import { eventsData } from "../../data/events";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 
 export const Events: FC<{}> = ({}) => {
   return (
@@ -8,29 +11,37 @@ export const Events: FC<{}> = ({}) => {
       <h2 className="text-3xl md:text-5xl font-bold text-primary-light pb-4">
         Events
       </h2>
-      <div className="flex flex-col space-y-3">
+      
+      <div className="flex flex-col w-full space-y-3">
         {eventsData.map((event) => {
           return (
             <div
               key={event.index}
               className={`flex ${
-                event.align === "left" ? "flex-row" : "flex-row-reverse"
-              } items-center p-5 w-full rounded-xl bg-primary/30 shadow-md`}
+                event.align === "left" ? "md:flex-row " : "md:flex-row-reverse "
+              } items-center  flex-col  p-5  rounded-xl bg-primary/30 shadow-md`}
             >
-              <div className="flex relative">
-                {event.eventImgs.map((imgUrl, i) => {
+              <div className="w-1/2 h-52 ">
+                <Carousel infiniteLoop autoPlay interval={2000} showArrows={false} showStatus={false} >
+                { 
+                event.eventImgs.map((imgUrl, i) => {
                   return (
+                    <div key={i} className="w-52 h-52" >
                     <Image
-                      key={i}
+                      
                       src={imgUrl}
-                      width={200}
-                      height={100}
+                     fill
                       alt={event.eventName}
+                     style={{objectFit:'contain'}}
                     />
+                    </div>
                   );
-                })}
+               })}
+                
+                </Carousel>
+                
               </div>
-              <span className="text-white">{event.eventDesc}</span>
+              <p className="text-white">{event.eventDesc}</p>
             </div>
           );
         })}
