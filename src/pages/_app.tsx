18baedfd9157174c3c53b151fb77bdd89bf4b6ad
himanisoni05 <himani.soni9099@gmail.com>
2023-01-13 23:58:16 +0530

    profile page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,7 +47,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 )}
                 <Component {...pageProps} />
                 {router.pathname.includes('/dashboard') ? (
-                    <DashboardNav />
+                    null
                 ) : (
                     <Footer />
                 )}
